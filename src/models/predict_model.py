84b9ba416fc9a82f56a412f5735e87c7a0cb195c
sofiane <sofiane.elfartass@gmail.com>
2024-04-26 19:14:05 +0200

    final 🎉

--- src/models/predict_model.py
+++ src/models/predict_model.py
@@ -8,5 +8,7 @@ def load_trained_model(model_path):
 def make_predictions(model, X):
     """Make predictions with a trained model."""
     predictions = model.predict(X)
-    return (predictions > 0.5).astype(int)
+    predicted_classes = (predictions > 0.5).astype(int)
+
+    return predictions, predicted_classes
 
